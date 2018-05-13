e8b87c734d9b27cbd523ee1efec6bb3a9ea99c64
Tianjian Wang <wangtjwork@gmail.com>
2018-05-13 16:44:27 -0500

    feat: Add input field to AddDeck

--- components/AddDeck.js
+++ components/AddDeck.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import CustomInput from './CustomInput';
 
 class AddDeck extends Component {
+  state = {
+    title: ''
+  }
+
+  handleChange = (newTitle) => {
+    this.setState({
+      title: newTitle
+    });
+  }
+
   render() {
+    const { title, handleChange } = this.state;
+
     return (
       <View style={{flex: 1, alignItems: 'center'}}>
         <Text>What is the title of your new deck?</Text>
+        <CustomInput
+          placeholder={'Deck Title'}
+          value={title}
+          handleChange={this.handleChange}
+        />
       </View>
     )
   }
