f1bea1dc7536aa1574988ce6831b52a8411b11c9
Tianjian Wang <wangtjwork@gmail.com>
2018-05-13 19:04:58 -0500

    feat: Complete Dashboard Component

--- components/DashBoard.js
+++ components/DashBoard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { gray } from '../utils/colors';
 
 class DashBoard extends Component {
   state = {
@@ -28,12 +29,39 @@ class DashBoard extends Component {
   }
 
   render() {
+    const decks = Object.keys(this.state).map((key) => this.state[key]);
+
     return (
-      <View style={{flex: 1, justifyContent: 'center'}}>
-        <Text>DashBoard</Text>
+      <View style={{flex: 1}}>
+        {decks.map((deck) => {
+          const size = deck.questions.length;
+          return (
+            <View key={deck.title} style={styles.deckContainer}>
+              <Text style={styles.deckTitle}>{deck.title}</Text>
+              <Text style={styles.deckSize}>{size} {size === 1 ? 'card' : 'cards'}</Text>
+            </View>
+          )
+        })}
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  deckContainer: {
+    height: 150,
+    borderBottomColor: gray,
+    borderBottomWidth: 2 * StyleSheet.hairlineWidth,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  deckTitle: {
+    fontSize: 25
+  },
+  deckSize: {
+    fontSize: 20,
+    color: gray
+  }
+})
 
 export default DashBoard
