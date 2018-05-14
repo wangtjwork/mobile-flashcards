434bf98113d15226c50189dbf134fbc1abe611ed
Tianjian Wang <wangtjwork@gmail.com>
2018-05-13 21:54:33 -0500

    feat: Move loadData to Dashboard

--- App.js
+++ App.js
@@ -1,73 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import { getDecks } from './utils/helpers';
-import { red } from './utils/colors';
+import { StyleSheet, Text, View, StatusBar, YellowBox } from 'react-native';
 import AddDeck from './components/AddDeck';
 import AddQuestion from './components/AddQuestion';
 import Dashboard from './components/Dashboard';
 import Deck from './components/Deck';
 import { Constants } from 'expo';
 import { purple } from './utils/colors';
+import { createBottomTabNavigator } from 'react-navigation';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
+const Tabs = createBottomTabNavigator({
+  AddDeck: {
+    screen: AddDeck
+  },
+  Dashboard: {
+    screen: Dashboard
+  }
+})
 
 export default class App extends React.Component {
-  state = {
-    decks: {},
-    loading: true
-  }
-
-  componentDidMount() {
-    getDecks()
-      .then((decks) => {
-        if (decks === null) {
-          decks = {
-            React: {
-              title: 'React',
-              questions: [
-                {
-                  question: 'What is React?',
-                  answer: 'A library for managing user interfaces'
-                },
-                {
-                  question: 'Where do you make Ajax requests in React?',
-                  answer: 'The componentDidMount lifecycle event'
-                }
-              ]
-            },
-            JavaScript: {
-              title: 'JavaScript',
-              questions: [
-                {
-                  question: 'What is a closure?',
-                  answer: 'The combination of a function and the lexical environment within which that function was declared.'
-                }
-              ]
-            }
-          };
-        }
-        this.setState({
-          decks,
-          loading: false
-        });
-      })
-  }
-
   render() {
-    const { decks, loading } = this.state;
-
-    if (loading) {
-      return (
-        <View style={styles.container}>
-          <Text style={{color: red}}>Loading...</Text>
-        </View>
-      )
-    }
-
     return (
       <View style={{flex: 1}}>
         <View style={{backgroundColor: purple, height: Constants.statusBarHeight}}>
           <StatusBar translucent backgroundColor={purple} barStyle='light-content' />
         </View>
-        <Deck deck={this.state.decks['React']}/>
+        <Tabs />
       </View>
     );
   }
--- components/DashBoard.js
+++ components/DashBoard.js
@@ -1,14 +1,65 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { gray } from '../utils/colors';
+import { getDecks } from '../utils/helpers';
+import { gray, red } from '../utils/colors';
 
 class Dashboard extends Component {
+  state = {
+    decks: {},
+    loading: true,
+  }
+
+  componentDidMount() {
+    getDecks()
+      .then((decks) => {
+        if (decks === null) {
+          decks = {
+            React: {
+              title: 'React',
+              questions: [
+                {
+                  question: 'What is React?',
+                  answer: 'A library for managing user interfaces'
+                },
+                {
+                  question: 'Where do you make Ajax requests in React?',
+                  answer: 'The componentDidMount lifecycle event'
+                }
+              ]
+            },
+            JavaScript: {
+              title: 'JavaScript',
+              questions: [
+                {
+                  question: 'What is a closure?',
+                  answer: 'The combination of a function and the lexical environment within which that function was declared.'
+                }
+              ]
+            }
+          };
+        }
+        this.setState({
+          decks,
+          loading: false
+        });
+      })
+  }
+
   render() {
-    const decks = Object.keys(this.props.decks).map((key) => this.props.decks[key]);
+    const { decks, loading } = this.state;
+
+    if (loading) {
+      return (
+        <View style={styles.container}>
+          <Text style={{color: red}}>Loading...</Text>
+        </View>
+      )
+    }
 
     return (
       <View style={{flex: 1}}>
-        {decks.map((deck) => {
+        {Object.keys(decks).map((deckTitle) => {
+          const deck = decks[deckTitle];
           const size = deck.questions.length;
           return (
             <View key={deck.title} style={styles.deckContainer}>
